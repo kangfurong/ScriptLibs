d70492f2a34cf6778a6794d6da74f4002fdd49bf
kangfurong <kangfurong@126.com>
2025-03-19 15:01:36 +0800

    Update 每天60s读懂世界.py

--- 每天60s读懂世界.py
+++ 每天60s读懂世界.py
@@ -22,8 +22,8 @@ info2 = f"""
 """
 
 # 发送分片推送  
-notify.send("每天60s读懂世界", info1 + "\n\n")
-notify.send("每天60s读懂世界", info2)
+#notify.send("每天60s读懂世界", info1 + "\n\n")
+#notify.send("每天60s读懂世界", info2)
 
 # 全文整段发送推送  
-# notify.send("每天60s读懂世界", content)
+notify.send("每天60s读懂世界", content)
