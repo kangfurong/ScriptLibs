f7951a1127a263f3db7e3d9026003ac00f96d15d
kangfurong <kangfurong@126.com>
2025-03-20 09:43:43 +0800

    Update 52chAutoSign.py

--- 52chAutoSign.py
+++ 52chAutoSign.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
         "Origin":"https://www.52ch.net",
         "Host":"www.52ch.net",
         #"Content-Length":"74",
-        "Referer":"https://www.52ch.net/dsu_paulsign-sign.html?needlogin=1&themecolor=F08200",
+        #"Referer":"https://www.52ch.net/dsu_paulsign-sign.html?needlogin=1&themecolor=F08200",
         "Content-Type": "application/x-www-form-urlencoded",
         "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MAGAPPX|6.4.0-6.4.0-164|iOS 15.4.1 iPhone 12|wachwl|BF204ADF-8D36-4C0E-AE55-D440C9510170|fb534a1a4cb8f3d2dd77b819412a5eea|f3acae52f0d4ac662dba63b38b095d30|9eb96e07f0d1d141cc8f6a34c1bf9613"
     }
