c048b039e96e422c82cb01bfeab286f7bf71cbe1
kangfurong <kangfurong@126.com>
2025-03-24 13:26:29 +0800

    Update 每天60s读懂世界.py

--- 每天60s读懂世界.py
+++ 每天60s读懂世界.py
@@ -1,29 +1,42 @@
-# @author Sten
-# 作者仓库:https://github.com/aefa6/QinglongScript.git
+# @author   frkang
 # 觉得不错麻烦点个star谢谢
 # 使用青龙自带的通知，某些推送不支持较长的文本推送，故默认分片推送，如果需要合并推送请将25和26行加#注释掉，29行去除#注释即可。
 import requests
 import json
 import notify
 
-url = 'https://60s.viki.moe/?encoding=text'
+#url = 'https://60s.viki.moe/?encoding=text'
+url = 'https://lzw.me/x/iapi/60s/?e=text'
 resp = requests.get(url)
 content = resp.text
-# 分片处理
-pieces = resp.text.split('\n', 8)
-content1 = '\n'.join(pieces[:8])  
-content2 = '\n'.join(pieces[8:])
 
-info1 = f"""
-{content1}   
-"""
-info2 = f"""
-{content2}   
-"""
+# 分割成多个子字符串
+substrings = content.split('\n')
+# 给每个子字符串加上序号并添加到新列表中
+numbered_substrings = []
+for i, sub in enumerate(substrings, start=1):
+    numbered_substring = f"{i}. {sub}"
+    numbered_substrings.append(numbered_substring)
+
+# 将新列表连接起来
+result = '\n'.join(numbered_substrings)
+
+
+# 分片处理
+#pieces = resp.text.split('\n', 8)
+#content1 = '\n'.join(pieces[:8])  
+#content2 = '\n'.join(pieces[8:])
+
+#info1 = f"""
+#{content1}   
+#"""
+#info2 = f"""
+#{content2}   
+#"""
 
 # 发送分片推送  
 #notify.send("每天60s读懂世界", info1 + "\n\n")
 #notify.send("每天60s读懂世界", info2)
 
 # 全文整段发送推送  
-notify.send("每天60s读懂世界", content)
+notify.send("每日要闻", result)
